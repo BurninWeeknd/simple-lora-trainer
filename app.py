dd4a64c417a69f592f6fe7d1658a56aadb7c5de7
Josh <jcalebk06@gmail.com>
2025-12-20 06:10:03 -0500

    Minor changes, proper port hosting, and visual changes

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from flask import Flask
 from blueprints.ui import ui_bp
 from blueprints.projects import projects_bp
 from blueprints.training import training_bp
+import os
+import secrets
 
 app = Flask(__name__)
-app.secret_key = "dev"
+app.secret_key = os.environ.get(
+    "FLASK_SECRET_KEY",
+    secrets.token_hex(32)
+)
+
 
 app.register_blueprint(ui_bp)
 app.register_blueprint(projects_bp)
 app.register_blueprint(training_bp)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(
+        host="127.0.0.1",
+        port=port,
+        debug=False,
+    )
